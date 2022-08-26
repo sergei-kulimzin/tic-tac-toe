ecc63eb443b9a3c61c50aeab4ac27ec15fcdd7c4
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-08-26 13:12:28 +0300

    init providers

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles.css';
+
+import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
