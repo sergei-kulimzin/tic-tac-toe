be6cebffa07c63a3595913f24407020023f2cca9
Kulimzin Sergei <kulimzin.sergei@gmail.com>
2022-09-04 21:11:29 +0300

    edit start game page styles

--- src/pages/StartGamePage/StartGamePage.tsx
+++ src/pages/StartGamePage/StartGamePage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FieldError, SubmitHandler, useForm } from 'react-hook-form';
+import classnames from 'classnames';
 
 import styles from './startGamePage.module.css';
 
@@ -11,7 +12,12 @@ function StartGamePage(): JSX.Element {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const { register, handleSubmit, getValues } = useForm<FormInputs>({
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<FormInputs>({
     mode: 'onTouched',
     defaultValues: {
       gameBoardSize: 3,
@@ -24,71 +30,97 @@ function StartGamePage(): JSX.Element {
     navigate('/game');
   };
 
+  const inputWrapperClassNames = (error: FieldError | undefined) =>
+    classnames({
+      [styles.inputWrapper]: true,
+      [styles.inputWrapperError]: error,
+    });
+
   return (
-    <div className={styles.startGamePage}>
-      <div className='container'>
-        <h1 className={styles.title}>Крестики - Нолики</h1>
-        <form className={styles.playersForm} onSubmit={handleSubmit(submitHandler)}>
+    <div className='container'>
+      <form className={styles.form} onSubmit={handleSubmit(submitHandler)}>
+        <div className={inputWrapperClassNames(errors.player1)}>
           <input
-            className={styles.inputPlayer}
+            className={styles.textInput}
             type='text'
-            placeholder='Имя первого игрока..'
+            id='player1'
             {...register('player1', {
               validate: {
-                isNotEmpty: (value) => value.trim() !== '' || 'Введите имя игрока',
+                isNotEmpty: (value) => value.trim() !== '' || 'поле не должно быть пустым',
                 isNotSame: (value) =>
-                  value.toLowerCase() !== getValues('player2').toLowerCase() || 'Одинаковые имена игроков',
+                  value.toLowerCase() !== getValues('player2').toLowerCase() || 'одинаковые имена игроков',
               },
             })}
           />
+          <label className={styles.label} htmlFor='player1'>
+            Имя первого игрока
+          </label>
+          {errors.player1 && <span className={styles.errorMessage}>{errors.player1.message}</span>}
+        </div>
 
+        <div className={inputWrapperClassNames(errors.player2)}>
           <input
             className={styles.inputPlayer}
             type='text'
-            placeholder='Имя второго игрока..'
+            id='player2'
             {...register('player2', {
               validate: {
-                isNotEmpty: (value) => value.trim() !== '' || 'Введите имя игрока',
+                isNotEmpty: (value) => value.trim() !== '' || 'поле не должно быть пустым',
                 isNotSame: (value) =>
-                  value.toLowerCase() !== getValues('player1').toLowerCase() || 'Одинаковые имена игроков',
+                  value.toLowerCase() !== getValues('player1').toLowerCase() || 'одинаковые имена игроков',
               },
             })}
           />
+          <label className={styles.label} htmlFor='player2'>
+            Имя второго игрока
+          </label>
+          {errors.player2 && <span className={styles.errorMessage}>{errors.player2.message}</span>}
+        </div>
 
-          <div className={styles.inputNumbers}>
-            <label htmlFor='gameBoardSize'>Размер игровой доски</label>
-            <input
-              className={styles.inputNumber}
-              type='number'
-              id='gameBoardSize'
-              {...register('gameBoardSize', {
-                validate: {
-                  isNotLessThanThree: (value) => value >= 3 || 'Должно быть не меньше 3',
-                  isNotLessThanWinCellsAmount: (value) =>
-                    value >= getValues('winCellsAmount') || 'Не должно быть меньше количества ячеек для победы',
-                },
-              })}
-            />
+        <div className={inputWrapperClassNames(errors.gameBoardSize)}>
+          <input
+            className={styles.inputNumber}
+            type='number'
+            id='gameBoardSize'
+            inputMode='numeric'
+            {...register('gameBoardSize', {
+              validate: {
+                isNotLessThanThree: (value) => +value >= 3 || 'значение не может быть меньше 3',
+                isNotLessThanWinCellsAmount: (value) =>
+                  +value >= getValues('winCellsAmount') || 'значение не может быть меньше количества ячеек для победы',
+              },
+            })}
+          />
+          <label className={styles.label} htmlFor='gameBoardSize'>
+            Размер игровой доски
+          </label>
+          {errors.gameBoardSize && <span className={styles.errorMessage}>{errors.gameBoardSize.message}</span>}
+        </div>
 
-            <label htmlFor='winCellsAmount'>Количество ячеек для победы</label>
-            <input
-              className={styles.inputNumber}
-              type='number'
-              id='winCellsAmount'
-              {...register('winCellsAmount', {
-                validate: {
-                  isNotLessThanThree: (value) => value >= 3 || 'Должно быть не меньше 3',
-                  isNotGreaterThanGameBoardSize: (value) =>
-                    value <= getValues('gameBoardSize') || 'Не должно быть больше размера игровой доски',
-                },
-              })}
-            />
-          </div>
-          <button className={styles.submitButton} type='submit'>
-            НАЧАТЬ ИГРУ
-          </button>
-        </form>
-      </div>
+        <div className={inputWrapperClassNames(errors.winCellsAmount)}>
+          <input
+            className={styles.inputNumber}
+            type='number'
+            id='winCellsAmount'
+            inputMode='numeric'
+            {...register('winCellsAmount', {
+              validate: {
+                isNotLessThanThree: (value) => +value >= 3 || 'значение не может быть меньше 3',
+                isNotGreaterThanGameBoardSize: (value) =>
+                  +value <= getValues('gameBoardSize') || 'значение не может быть больше размера игровой доски',
+              },
+            })}
+          />
+          <label className={styles.label} htmlFor='winCellsAmount'>
+            Ячеек для победы
+          </label>
+          {errors.winCellsAmount && <span className={styles.errorMessage}>{errors.winCellsAmount.message}</span>}
+        </div>
+
+        <button className={styles.button} type='submit'>
+          НАЧАТЬ ИГРУ
+        </button>
+      </form>
     </div>
   );
 }
